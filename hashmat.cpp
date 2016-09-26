80241ecc60c87d0e2673cb2fdfb8e840622d55bd
rhegil <rhegilxyxy@gmail.com>
2016-09-26 20:43:57 +0800

    Add files via upload

---
+++ hashmat.cpp
@@ -0,0 +1,23 @@
+//BOLECHE, RHEGIL P.
+//201510973	
+//CPEPROG2LA2
+
+
+#include<iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+int main(){
+	
+	
+	int allies, enemies, diff;
+	
+	cout<<"Hashmat Allies:";
+	cin>>allies;
+	cout<<"Hashmat Enemy:";
+	cin>>enemies;
+	
+	diff = abs(allies-enemies);
+	cout<<"Difference of number: "<<diff;
+}
